a8da334a2069bb0a320e5d79d8d9ef7c95c13675
Lucas Rodriguez <lucas.rodriguez@outlook.com>
2024-01-15 10:13:54 -0300

    Store and return results in volume_vector function

--- foo.R
+++ foo.R
@@ -8,9 +8,11 @@ volume <- function(r) {
 
 # Function to compute the volumes of the spheres with radius r, r^2 and r^3
 volume_vector <- function(r) {
+  volumes <- c()
   for (i in 1:3){
-    volume(r)
+    volumes <- c(volumes, volume(r^i))
   }
+  return(volumes)
 }
 
 # Run volume_vector(r) and print the volumes of the spheres with radius r, r^2 and r^3
